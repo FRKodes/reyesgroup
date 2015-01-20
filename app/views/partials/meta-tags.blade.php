b67388db51e843111227b3c9933973b5f483460b
frkodes <frkalderon@gmail.com>
2015-01-20 14:46:34 -0600

    Changing clients images

--- app/views/partials/meta-tags.blade.php
+++ app/views/partials/meta-tags.blade.php
@@ -1,13 +1,13 @@
 <meta charset="UTF-8">
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-<meta name="description" content="Reyes Group, sitio web del grupo que desarrolla el concepto de reyes salón cantina.">
-<meta name="keywords" content="reyes, reyes salon, cantina, cantina">
+<meta name="description" content="Reyes Group, sitio web del grupo que desarrolla el concepto de reyes salón cantina. Los creadores del original shot de alacrán desde Guadalajara Jalisco, México.">
+<meta name="keywords" content="reyes, reyes salon, cantina, shot de alacrán, mezcal, shot de mezcal">
 <meta name="expires" content="tue, 07 Apr 2015 09:45:00 GMT">
 <meta property="og:locale" content="es_MX" >
 <meta property="og:type" content="website" >
 <meta property="og:title" content="Reyes Group" >
-<meta property="og:description" content="Reyes Group, sitio web del grupo que desarrolla el concepto de reyes salón cantina." >
+<meta property="og:description" content="Reyes Group, sitio web del grupo que desarrolla el concepto de reyes salón cantina.Los creadores del original shot de alacrán desde Guadalajara Jalisco, México." >
 <meta property="og:url" content="http://www.reyesgroup.com.mx" >
 <meta property="og:site_name" content="Reyes Group" >
 <link rel="canonical" href="http://www.reyesgroup.com.mx" >
