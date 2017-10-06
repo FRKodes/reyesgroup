9176f64a0d71ef2247146b40b5c0c984cb0dd2e8
frkodes <frkalderon@gmail.com>
2017-10-06 15:32:36 -0500

    Services file update, getting rid from mandrill key

--- app/config/services.php
+++ app/config/services.php
@@ -1,10 +1,6 @@
 <?php
 
 return [
-	'mandrill' => [
-		'secret' => $_ENV['MANDRILL_SECRET']
-	],
-
 	'mailgun' => [
 	    'domain' => $_ENV['MAILGUN_DOMAIN'],
 	    'secret' => $_ENV['MAILGUN_KEY'],
